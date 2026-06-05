b88862d9e856a73d93e30077bc3208c75780f92f
KidCarmi <orelcarmi@outlook.com>
2026-06-05 17:10:29 +0300

    Complete SimpleSAMLphp interop fixture

---
+++ .github/idp/simplesamlphp/saml20-sp-remote.php
@@ -0,0 +1,9 @@
+<?php
+$metadata['https://proxy.example.test/culvert'] = [
+    'AssertionConsumerService' => [
+        [
+            'Location' => 'https://proxy.example.test/culvert/auth/saml/callback',
+            'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+        ],
+    ],
+];
